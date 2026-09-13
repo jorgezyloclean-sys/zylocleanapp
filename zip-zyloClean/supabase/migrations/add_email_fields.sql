0068619c0400fd575ba83cb7c4816781a1ceb30d
jorgezyloclean-sys <jorgezyloclean@gmail.com>
2026-09-13 02:49:35 -0300

    Delete zip-zyloClean directory

--- zip-zyloClean/supabase/migrations/add_email_fields.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add email fields to clients and staff
--- Run this in Supabase SQL Editor
-
-ALTER TABLE public.clients ADD COLUMN IF NOT EXISTS email text;
-ALTER TABLE public.staff ADD COLUMN IF NOT EXISTS email text;
